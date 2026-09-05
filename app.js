677e3c584c30067fd092e06cde3f8b7929c32ed0
digitalivepandu-arch <digitalivepandu@gmail.com>
2026-09-05 21:46:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 // GANTI DENGAN URL WEB APP GAS ANDA YANG AKTIF
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzF9ak3b2QVPe9mmP5O21UV919NuFSKPWZv06AZt_btNXvuWEub43QiflC-9o88SaHsPg/exec"; 
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz4sSLd5FHiGOz0sa96-B0lHfB2rXRqFWYx56KFm6QyeV9eBt3qDGJpwdfcJ46F5aFX5Q/exec"; 
 
 let compressedBase64 = null;
 let loadedFieldsConfig = [];
