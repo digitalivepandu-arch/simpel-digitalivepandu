758fa4c5e81e634caa588abc2c58570e2468416d
digitalivepandu-arch <digitalivepandu@gmail.com>
2026-09-07 20:11:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let compressedBase64 = null;
 let loadedFieldsConfig = [];
 
 // Menggunakan URL Web App Google Apps Script secara langsung
-let GAS_URL = "https://script.google.com/macros/s/AKfycbyc_myCNkETLyI5Tsfu1JwLySn62IUUdM-PMDJC4EHcG5bNdSin5Mk4fA5R6maE7X26RQ/exec";
+let GAS_URL = "https://script.google.com/macros/s/AKfycbyJ3MJrjL6uYChYJ_8jynHKYaY7w49YsUpFWC33C2HzRKXbFa2yIHSxO5WG0K9ZVAr-Mw/exec";
 
 // Mendengarkan hasil foto dari window kamera pop-up direct
 window.addEventListener('message', function(event) {
