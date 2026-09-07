4089f988c75736d571050ef422a98a03645773f3
digitalivepandu-arch <digitalivepandu@gmail.com>
2026-09-07 16:15:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let compressedBase64 = null;
 let loadedFieldsConfig = [];
 
 // Mengambil GAS_URL dari localStorage, jika kosong gunakan URL web app default Anda
-let GAS_URL = localStorage.getItem('custom_gas_url') || "https://script.google.com/macros/s/AKfycbwnWh1ttSPVTW9C4rpIMs9mtUvWPPIa_1znmqZb8NRGWT1KZpqEdSaDvfP8KSpkRzAqZQ/exec";
+let GAS_URL = localStorage.getItem('custom_gas_url') || "https://script.google.com/macros/s/AKfycbxAr0cWFcgCtUZyKWXAGfxNpDroa1h4QymxFHjF6zZ-X44yasNl1PnMl-tslafIJxhKWg/exec";
 
 // Fungsi untuk mengganti URL GAS baru dari dashboard (bisa dipicu lewat Input Text & Tombol Save)
 function updateGasUrl(newUrl) {
