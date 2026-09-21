1c6e9a959ba6c95a8d068d7cecd025d46ff82774
digitalivepandu-arch <digitalivepandu@gmail.com>
2026-09-22 00:29:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let compressedBase64 = null;
 let loadedFieldsConfig = [];
 
 // Menggunakan URL Web App Google Apps Script secara langsung
-let GAS_URL = "https://script.google.com/macros/s/AKfycbyx0GZLZ0PxlyO8B25UJDm2QWlhEo1ZhwKhyLVyVstevsMIKjbbKEIKMpRVSwhQZ-prVg/exec";
+let GAS_URL = "https://script.google.com/macros/s/AKfycbz-VoFR5cdY2WL5ZOmcYq-PrYr8cipRjZAT6-DdNUBWnM0n0qBMxXpNd8IaqasDYAU3Dg/exec";
 let isAbsenSubmitting = false; // Pengunci Spam Click
 
 window.addEventListener('load', () => {
